230645eb53b6b1ee036118d70109d896ed430a31
Stephen Hulme <sh@salt.ac.za>
2020-12-01 17:54:16 +0200

    feat: return the parameters of each Commit

--- rubyhappened.rb
+++ rubyhappened.rb
@@ -13,15 +13,24 @@ class Commit
     @message
   end
 
+  # return a semver object containing major, minor, patch, prerelease, buildmetadata
   def semver
-    @refs.match(/(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)(?:-(?<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/)
+    @refs.match(/(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)(?:-(?<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/).named_captures
+  end
+
+  # return Git-By-Numbers object containing breaking, type, scope, description, body
+  def parameters
+    parameters_match = @message.match(/(?<breaking>breaking )?(?<type>[0-9a-zA-Z]+): ?(?<description>.*)(\n?)(?<body>.+)?/)
+    if parameters_match != nil
+      parameters_match.named_captures
+    end
   end
 end
 
 # make an object
 # Objects are created on the heap
 
-git_log_command = "git log --pretty='format:%d|%s' --reverse"
+git_log_command = "git log --pretty='format:%d|%s'"
 git_log = `#{git_log_command}`
 # puts git_log
 # puts ''
@@ -36,5 +45,6 @@ end
 
 # puts commits.inspect
 
-first_tagged_commit = commits.first{|x| x.refs.include?("tag")}
-puts first_tagged_commit.semver.named_captures
+last_version_index = commits.find_index{ | each | each.semver }
+puts commits[last_version_index].semver
+puts commits.map{ | each | each.type }
